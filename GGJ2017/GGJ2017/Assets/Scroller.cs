9a9fe1305664d239387b8379901542bad621b13b
Manfroi <hmanfroi@hotmail.com>
2017-01-21 17:39:15 -0200

    Music file change, History Scene create

---
+++ GGJ2017/GGJ2017/Assets/Scroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scroller : MonoBehaviour {
+
+	public string[] intro;
+	public float off;
+	public float speed = 100;
+
+	public int movementspeed=100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.Translate (Vector3.up * 300 * Time.deltaTime);
+		} else {
+			transform.Translate (Vector3.up * movementspeed * Time.deltaTime);
+		}
+
+		if( transform.position.y >= 1200){
+			SceneManager.LoadScene ("Ingame");
+		}
+	}
+}
