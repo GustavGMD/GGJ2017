b63cce5cf490151e3c788a498e0f073220948aff
GustavGMD <gromanmarangon@gmail.com>
2017-01-20 23:34:23 -0200

    Gustavo Test Scene
    Energy Pulse Objects/Scripts

---
+++ GGJ2017/GGJ2017/Assets/__Project/Scripts/EnergyPulseParticle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyPulseParticle : MonoBehaviour {
+
+    public event Action<GameObject> onDestroy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
