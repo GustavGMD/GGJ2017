cb4da81afd408c4d272d86a1402a08e882734904
Manfroi <hmanfroi@hotmail.com>
2017-01-22 11:22:12 -0200

    Sound buttons

---
+++ GGJ2017/GGJ2017/Assets/ButtonAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAudio : MonoBehaviour {
+
+	public Button myButton;
+
+	// Use this for initialization
+	void Start () {
+		myButton.GetComponent<Button>().onClick.AddListener(() => { playEffect(); });
+	}
+	
+	void playEffect(){
+		print ("PLay");
+		AudioManagerSingleton.instance.PlaySound (
+			AudioManagerSingleton.AudioClipName.BUTTON_1, AudioManagerSingleton.AudioType.SFX, false, 1);
+	}
+}
