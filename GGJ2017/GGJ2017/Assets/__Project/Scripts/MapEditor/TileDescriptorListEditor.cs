34460edf67c75fc0cff0ff517506e4c5f2a05818
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2017-01-22 13:10:53 -0200

    tiles de chão e parede no editor

---
+++ GGJ2017/GGJ2017/Assets/__Project/Scripts/MapEditor/TileDescriptorListEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TileDescriptorList))]
+public class TileDescriptorListEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        TileDescriptorList myScript = (TileDescriptorList)target;
+        if (GUILayout.Button("Add Selected Prefab"))
+        {
+            GameObject currentObject = Selection.activeGameObject;
+            if(currentObject == null)
+            {
+                Debug.Log("nenhum objeto selecionado");
+                return;
+            }
+
+            if(PrefabUtility.GetPrefabObject(currentObject) == null)
+            {
+                Debug.Log("Selecione um prefab");
+                return;
+            }
+            MapEditorTileDescriptor[] newTileList = new MapEditorTileDescriptor[myScript.tileDescriptors.Length + 1];
+            for (int i  = 0; i < myScript.tileDescriptors.Length; i++)
+            {
+                newTileList[i] = myScript.tileDescriptors[i];
+            }
+
+            MapEditorTileDescriptor tileDescriptor = new MapEditorTileDescriptor();
+            tileDescriptor.prefab = currentObject;
+            tileDescriptor.type = currentObject.name;
+
+            newTileList[myScript.tileDescriptors.Length] = tileDescriptor;
+
+            myScript.tileDescriptors = newTileList;
+            Debug.Log(  "Adicionado: " + tileDescriptor.type );
+           // myScript.BuildObject();
+        }
+    }
+}
